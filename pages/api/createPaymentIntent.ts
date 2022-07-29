81ab97c01008e28eb74eed1e0bfdb0eef4169e23
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-07-29 12:12:26 -0600

    Add product metadata for future webhook functionality

--- pages/api/createPaymentIntent.ts
+++ pages/api/createPaymentIntent.ts
@@ -5,12 +5,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { amount } = req.body;
+  const { amount, product } = req.body;
   const paymentIntent = await stripe.paymentIntents.create({
     amount: amount,
     currency: "usd",
     automatic_payment_methods: {
       enabled: true,
+    },
+    metadata: {
+      product: product,
     },
   });
   res.send({ clientSecret: paymentIntent.client_secret });
