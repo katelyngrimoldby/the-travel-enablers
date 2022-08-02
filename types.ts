92591785db6a91ae951037f007336801ad29d1e8
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-08-02 13:50:37 -0600

    Tweak field names

--- types.ts
+++ types.ts
@@ -6,13 +6,13 @@ export interface TypeGroupTripFields {
   slug: Contentful.EntryFields.Symbol;
   dates: Contentful.EntryFields.Symbol;
   initialDescription: CFRichTextTypes.Block | CFRichTextTypes.Inline;
-  itenerary: CFRichTextTypes.Block | CFRichTextTypes.Inline;
+  itinerary: CFRichTextTypes.Block | CFRichTextTypes.Inline;
   closingDescription: CFRichTextTypes.Block | CFRichTextTypes.Inline;
   packageDetails: CFRichTextTypes.Block | CFRichTextTypes.Inline;
   packageNames: Contentful.EntryFields.Symbol[];
   packagePrices: Contentful.EntryFields.Symbol[];
   deposit: Contentful.EntryFields.Integer;
-  test: Contentful.Asset[];
+  images: Contentful.Asset[];
   thumbImg: Contentful.Asset;
 }
 
