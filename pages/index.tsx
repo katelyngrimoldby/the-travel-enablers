d2d4321ca3c7fd0dadb329a2189cfda2ee9b27c5
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-08-02 12:50:06 -0600

    Add content injection & add hero component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,38 @@
 import type { NextPage } from "next";
+import { createClient } from "contentful";
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
+import { TypeGroupTrip, TypeGroupTripFields } from "../types";
 import Button from "../components/Button";
-import PaymentComponent from "../components/PaymentComponent";
+import Hero from "../components/Hero";
+import TripCard from "../components/TripCard";
 import Divider from "../icons/Divider";
 import heroImg from "../public/hero-img.jpg";
 import styles from "../styles/index.module.scss";
 
-const Home: NextPage = () => {
+type PageProps = {
+  trips: TypeGroupTrip[];
+};
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: `${process.env.NEXT_PUBLIC_SPACE_ID}`,
+    accessToken: `${process.env.NEXT_PUBLIC__ACCESS}`,
+  });
+
+  const res = await client.getEntries<TypeGroupTripFields>({
+    content_type: "groupTrip",
+  });
+
+  return {
+    props: {
+      trips: res.items,
+    },
+  };
+}
+
+const Home: NextPage<PageProps> = ({ trips }) => {
   return (
     <>
       <Head>
@@ -16,25 +40,16 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <header className={styles.hero}>
-          <div className={styles.heroContent}>
-            <h1>THE TRAVEL ENABLERS</h1>
-            <span>How can we enable you?</span>
-            <Divider />
-            <Button location="#" buttonType="main" value="Take the Quiz" />
-          </div>
-          <div className={styles.imgWrapper}>
-            {/* <Image
-              src={heroImg}
-              alt=""
-              className={styles.heroImg}
-              layout="responsive"
-              priority={true}
-            /> */}
-          </div>
-        </header>
+        <Hero imgSrc={heroImg}>
+          <h1>THE TRAVEL ENABLERS</h1>
+          <span>How can we enable you?</span>
+          <Divider />
+          <Button location="#" buttonType="main" value="Take the Quiz" />
+        </Hero>
         <div>
-          <PaymentComponent />
+          {trips.map((e: TypeGroupTrip) => {
+            return <TripCard key={e.sys.id} trip={e} />;
+          })}
         </div>
       </main>
     </>
