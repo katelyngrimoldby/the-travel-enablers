bb1b5d45e554843acf205ee4e72798d81fd774bd
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-08-15 04:56:13 -0600

    Add article types

--- types.ts
+++ types.ts
@@ -1,6 +1,15 @@
 import * as CFRichTextTypes from "@contentful/rich-text-types";
 import * as Contentful from "contentful";
 
+export interface TypeArticleFields {
+  title: Contentful.EntryFields.Symbol;
+  slug: Contentful.EntryFields.Symbol;
+  coverImage: Contentful.Asset;
+  article: CFRichTextTypes.Block | CFRichTextTypes.Inline;
+}
+
+export type TypeArticle = Contentful.Entry<TypeArticleFields>;
+
 export interface TypeGroupTripFields {
   title: Contentful.EntryFields.Symbol;
   slug: Contentful.EntryFields.Symbol;
