4114eacc994d3ac00c5ec200f79e55485a07f06a
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-08-15 04:46:52 -0600

    Add services and improve upcoming trips

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,8 @@ import Hero from "../components/Hero";
 import TripCard from "../components/TripCard";
 import Divider from "../icons/Divider";
 import heroImg from "../public/hero-img.jpg";
+import gtImg from "../public/group-trip-img.jpg";
+import ctImg from "../public/custom-trip-img.jpg";
 import styles from "../styles/index.module.scss";
 
 type PageProps = {
@@ -60,11 +62,75 @@ const Home: NextPage<PageProps> = ({ trips }) => {
           <Divider />
           <Button location="#" buttonType="main" value="Take the Quiz" />
         </Hero>
-        <div>
-          {sortedTrips.map((e: TypeGroupTrip) => {
-            return <TripCard key={e.sys.id} trip={e} />;
-          })}
-        </div>
+
+        <section className={styles.light}>
+          <h2>What We Do</h2>
+
+          <section className={styles.content}>
+            <h3>Small Group Trips</h3>
+
+            <div className={styles.left}>
+              <div className={styles.sectionImg}>
+                <Image src={gtImg} alt="Group sitting under table on beach" />
+              </div>
+
+              <div className={styles.textWrapper}>
+                <p>
+                  Let’s be honest, some places are just better in a group
+                  dynamic! We create boutique travel experiences so that if
+                  you’re a duo or solo traveler, you will have the opportunity
+                  to share the wonders of the world in our small groups hosted &
+                  curated by Mandy McPherson.
+                </p>
+                <p>
+                  She has traveled the globe and from her years of exploring
+                  does all the research, planning, booking and organizing. From
+                  active adventures to luxurious long weekends away, at Travel
+                  Enablers we offer an array of bespoke vacations. Everything is
+                  thought of: accommodations, transport, tours, must see/eat/do
+                  lists and even off the beaten path recommendations. We even
+                  offer payment plans so that no matter your budget, everyone
+                  can make their travel dreams a reality.
+                </p>
+                <Button buttonType="light" location="/" value="View Trips" />
+              </div>
+            </div>
+          </section>
+          <section className={styles.content}>
+            <h3>Custom Trip Planning</h3>
+            <div className={styles.right}>
+              <div className={styles.sectionImg}>
+                <Image
+                  src={ctImg}
+                  alt="View of breach from behind palm trees"
+                />
+              </div>
+              <div className={styles.textWrapper}>
+                <p>
+                  Maybe you don’t have the time or desire to plan out your next
+                  vacation. We can handle as much or as little as you like. We
+                  are The Travel Enablers, after all! Is your family planning a
+                  road trip down the Pacific Coast Highway or you want help with
+                  your honeymoon to New Zealand or is it a friends’ trip to
+                  Bali? You want help getting the best deals, not worrying about
+                  making all the reservations and having the gift of time handed
+                  right to you. Take the “travel test” and choose your level of
+                  service and we take care of the rest.
+                </p>
+                <Button buttonType="light" location="/" value="Take the Quiz" />
+              </div>
+            </div>
+          </section>
+        </section>
+        <section className={styles.dark}>
+          <h2>Upcoming Trips</h2>
+          <div className={styles.cardWrapper}>
+            {sortedTrips.map((e: TypeGroupTrip) => {
+              return <TripCard key={e.sys.id} trip={e} buttonType="dark" />;
+            })}
+          </div>
+          <Button buttonType="main" location="/trips" value="View All Trips" />
+        </section>
       </main>
     </>
   );
