16db5a318fde3bd259eb73620e69061942cf1007
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-08-17 18:19:07 -0600

    Add prop for nav functionality

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,19 @@
-import Link from 'next/link';
-import styles from '../styles/Button.module.scss';
+import Link from "next/link";
+import styles from "../styles/Button.module.scss";
 
 type ButtonProps = {
   buttonType: string;
   location: string;
   value: string;
+  onClick?: () => void;
 };
 
-const Button = ({ buttonType, location, value }: ButtonProps) => {
+const Button = ({ buttonType, location, value, onClick }: ButtonProps) => {
   return (
     <Link href={location}>
-      <a className={styles[buttonType]}>{value}</a>
+      <a className={styles[buttonType]} onClick={onClick}>
+        {value}
+      </a>
     </Link>
   );
 };
