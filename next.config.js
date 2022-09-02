7b040605e2c494915cb44197828f261eb33049f7
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-09-02 12:07:07 -0600

    Tweak config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const withOptimizedImages = require("next-optimized-images");
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     disableStaticImages: true,
-    loader: "custom",
     domains: ["images.ctfassets.net"],
   },
 };
