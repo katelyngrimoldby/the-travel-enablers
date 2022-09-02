aba18ce4e4a048268f16bbf90a697033b187227f
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-09-02 11:29:27 -0600

    Add success page for contact

---
+++ pages/form-submitted.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Hero from "../components/Hero";
+import heroImg from "../public/hero-img.jpg";
+import Divider from "../icons/Divider";
+
+const FormSubmit: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Purchase Completed | The Travel Enablers</title>
+      </Head>
+      <main>
+        <Hero imgSrc={heroImg}>
+          <h1>THANK YOU FOR YOUR MESSAGE</h1>
+          <span>We&apos;ll get back to you as soon as possible!</span>
+          <Divider />
+        </Hero>
+      </main>
+    </>
+  );
+};
+
+export default FormSubmit;
