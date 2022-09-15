349f9d2417dc6a2521a25fdfd9edfa8f2fb3817f
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-09-14 18:52:11 -0600

    Update env tag

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -12,7 +12,7 @@ import styles from "../../styles/[articleSlug].module.scss";
 
 const client = createClient({
   space: `${process.env.NEXT_PUBLIC_SPACE_ID}`,
-  accessToken: `${process.env.NEXT_PUBLIC__ACCESS}`,
+  accessToken: `${process.env.NEXT_PUBLIC_ACCESS}`,
 });
 
 export const getStaticPaths = async () => {
