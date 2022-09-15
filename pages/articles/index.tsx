45b5792b4427e808f345b10a95e79cb5b5e1ceef
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-09-14 18:52:19 -0600

    Update env tag

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -16,7 +16,7 @@ type PageProps = {
 export async function getStaticProps() {
   const client = createClient({
     space: `${process.env.NEXT_PUBLIC_SPACE_ID}`,
-    accessToken: `${process.env.NEXT_PUBLIC__ACCESS}`,
+    accessToken: `${process.env.NEXT_PUBLIC_ACCESS}`,
   });
 
   const articles = await client.getEntries<TypeArticleFields>({
