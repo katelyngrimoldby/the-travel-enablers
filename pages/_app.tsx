6b0333b381cb9068abaa8b6f70c477bc165ae13f
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-09-14 18:53:17 -0600

    Integrate Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,45 @@
-import '../styles/globals.scss';
-import type { AppProps } from 'next/app';
-import Layout from '../components/Layout';
+import { useState } from "react";
+import type { AppProps } from "next/app";
+import Script from "next/script";
+import Layout from "../components/Layout";
+import CookieBanner from "../components/CookieBanner";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [result, setResult] = useState<boolean | null>(null);
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {result == true && (
+        <>
+          <Script
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA}`}
+          />
+          <Script id="analytics" strategy="lazyOnload">
+            {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${process.env.NEXT_PUBLIC_GA}');
+  console.log('${process.env.NEXT_PUBLIC_GA}')`}
+          </Script>
+        </>
+      )}
+      <Layout>
+        {result == null && (
+          <CookieBanner
+            accept={() => {
+              setResult(true);
+            }}
+            reject={() => {
+              setResult(false);
+            }}
+          />
+        )}
+
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
